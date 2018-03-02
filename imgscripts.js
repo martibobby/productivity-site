b2696d54f1eede744dac83daf704e99d243d6a6a
Bob <martibobby@gmail.com>
2018-03-02 09:53:38 -0600

    polishing the image gallery

--- imgscripts.js
+++ imgscripts.js
@@ -1,10 +1,11 @@
 function enlargeImg(picnum) {
     var bigImgHTML = document.getElementById("largeimg");
+    bigImgHTML.innerHTML = "";
     this.picnm = picnum;
     var pic = document.createElement("IMG");
     pic.src = "images/" + picnum;
     console.log(pic.src);
 
-    bigImgHTML.innerHTML = pic;
+    bigImgHTML.appendChild(pic);
     console.log(picnum);
 }
